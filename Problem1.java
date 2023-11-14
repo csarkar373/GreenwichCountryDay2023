afaf8d7474b16f4d765e2dd8e026dfb2bc191d27
Chandan Sarkar <csarkar373@gmail.com>
2023-11-13 20:01:54 -0500

    Added trim() to all input reads

--- Problem1.java
+++ Problem1.java
@@ -5,9 +5,9 @@ public class Problem1 {
     public static void main(String [] args) throws Exception {
         File f = new File("input1.txt");
         Scanner scan = new Scanner(f);
-        int m1 = Integer.parseInt(scan.nextLine());
-        int m2 = Integer.parseInt(scan.nextLine());
-        int percent = Integer.parseInt(scan.nextLine());
+        int m1 = Integer.parseInt(scan.nextLine().trim());
+        int m2 = Integer.parseInt(scan.nextLine().trim());
+        int percent = Integer.parseInt(scan.nextLine().trim());
         double total = (m1 + m2) + percent/100.0 * (m1 + m2);
         System.out.println(Math.round(total));
     }
